cae817059d01bd26dd0a72d73176dc5173be8314
MatheusN15 <matheusmn1519@gmail.com>
2021-08-29 19:53:23 -0300

    alterado Exercicios

--- src/br/com/healthtrack/Main.java
+++ src/br/com/healthtrack/Main.java
@@ -3,6 +3,14 @@ package br.com.healthtrack;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
+    	Exercicio exercicio1 = new Exercicio("caminhada", 50, Intensidade.MODERADA, 0, TipoExercicio.CAMINHADA);
+    	System.out.println(exercicio1.calcularCalorias(70, 1.90));
+    	
+    	Exercicio exercicio2 = new Exercicio("natacao", 50, Intensidade.MODERADA, 0, TipoExercicio.NATACAO);
+    	System.out.println(exercicio2.calcularCalorias(70, 1.90));
+    	
+    	Exercicio exercicio3 = new Exercicio("musculação", 50, Intensidade.MODERADA, 0, TipoExercicio.MUSCULACAO);
+    	System.out.println(exercicio3.calcularCalorias(80, 1.90));
     }
 }
